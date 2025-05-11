d6cfd6fdc7cbf765b7c927856f2ac35f94de280e
ryo-k-k <mayosio29@gmail.com>
2025-05-11 14:24:10 +0900

    Update keyball39.c

--- qmk_firmware/keyboards/keyball/keyball39/keyball39.c
+++ qmk_firmware/keyboards/keyball/keyball39/keyball39.c
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-#include "keyball.h"
+#include "lib/keyball/keyball.h"
 
 //////////////////////////////////////////////////////////////////////////////
 
